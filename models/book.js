a8fec43b4b796edaffa32834671f946b150d8836
Kevin Suh <kevinsuh2018@u.northwestern.edu>
2020-03-26 14:46:38 -0500

    initial directory strutuce

---
+++ models/book.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  authors: {
+    type: String,
+    default: ""
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+  //link to the book's thumbnail image
+  image: {
+    type: String,
+    default: "",
+  },
+  //link to the book's information page
+  link: {
+    type: String,
+    default: "",
+  },
+});
+
+const Books = mongoose.model("Recipe", bookSchema);
+
+module.exports = Books;
