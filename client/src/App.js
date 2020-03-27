3c191ebc1d6f9ccd5dab8b965a22d45b13b93e93
Kevin Suh <kevinsuh2018@u.northwestern.edu>
2020-03-26 22:18:53 -0500

    fatal bug, need to reinitialize

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header";
 import { Router } from 'express';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
